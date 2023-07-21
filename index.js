efbe7c457e1de43151f96bfa70ae057dc7875065
Chris <chrisextramail2@gmail.com>
2023-07-21 16:37:34 +0200

    neue ngin config, back depe ändet6

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+// index.js (or app.js)
+
+const express = require('express');
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://christianfleischhauer007:rPzDVDi7YUgsN7vj@cluster0.wimqjvk.mongodb.net/';
+const app = express();
+
+// Load environment variables from .env file (optional but recommended)
+require('dotenv').config();
+
+// MongoDB connection
+const mongoURI = process.env.MONGO_URL; // Use the environment variable for the MongoDB connection URL
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
@@ -9,6 +18,9 @@ mongoose.connect(mongoURI, {
   .then(() => {
     console.log('Connected to MongoDB');
     // Start your server or do other operations after successful connection.
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
   })
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err.message);
