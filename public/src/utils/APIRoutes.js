eafe1994d872b92970be26992b9ba60435ab0cb3
Chris <chrisextramail2@gmail.com>
2023-07-24 10:10:22 +0200

    ngin conf und api rout geändert

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "http://10.100.0.56:5000";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
